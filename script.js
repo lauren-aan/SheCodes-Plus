1d3230c25f373aecd161c575759793ea8f85fcb1
Lauren Grant <126265030+lauren-innova@users.noreply.github.com>
2023-05-02 20:25:06 +0100

    Finally got it working!

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Time and Date Function //
-function dateTime(data) {
-  let dateRT = data.getDate();
-  let hours = data.getHours();
+function formatTime(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = data.getMinutes();
+  let minutes = time.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let year = data.getFullYear();
+  let year = time.getFullYear();
 
   let days = [
     "Sunday",
@@ -20,7 +20,7 @@ function dateTime(data) {
     "Friday",
     "Saturday",
   ];
-  let day = days[data.getDay()];
+  let day = days[time.getDay()];
 
   let months = [
     "January",
@@ -36,88 +36,41 @@ function dateTime(data) {
     "November",
     "December",
   ];
-  let month = months[data.getMonth()];
-
-  let date = document.querySelector("#todaysDate");
-  date.innerHTML = `${day}, ${dateRT} ${month} ${year}`;
-
-  let time = document.querySelector("#currentTime");
-  time.innerHTML = `${hours}:${minutes}`;
+  let month = months[time.getMonth()];
 
   let greeting = document.querySelector("#greeting");
   if (hours < 12) {
-    greeting.innerHTML = "good morning";
+    greeting.innerHTML = "Good morning";
   } else {
     if (hours > 18) {
-      greeting.innerHTML = "good evening";
+      greeting.innerHTML = "Good evening";
     } else {
-      greeting.innerHTML = "good afternoon";
+      greeting.innerHTML = "Good afternoon";
     }
   }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-// Sending info to Time and Date Function //
-let currentTime = new Date();
-let dateElement = dateTime(currentTime);
-
-// Enter location button //
-
-function enterLocationButton(event) {
-  event.preventDefault();
-  let enterLocation = document.querySelector("#searchInput");
-
-  let displayedLocation = document.querySelector("#locationEntered");
-  displayedLocation.innerHTML = enterLocation.value;
-
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let searchCity = enterLocation.value;
-  let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=${units}`;
-  axios.get(url).then(showWeather);
+function displayTemperature(response) {
+  console.log(response.data.time);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let locationElement = document.querySelector("#location");
+  locationElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#todaysDate");
+  dateElement.innerHTML = formatTime(response.data.time * 1000);
 }
 
-function showWeather(response) {
-  console.log(response);
-  let newTemp = document.querySelector("#convertTemp");
-  let temperature = Math.round(response.data.main.temp);
-  newTemp.innerHTML = `${temperature}`;
+let key = "a3co8cfc69t20f3a05200f0a3ac4b3e8";
+let query = "Paris";
+let units = "metric";
+let url = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${key}&units=${units}`;
 
-  let newHumidity = document.querySelector("#humidity");
-  newHumidity.innerHTML = response.data.main.humidity;
-
-  let newWind = document.querySelector("#wind");
-  newWind.innerHTML = Math.round(response.data.wind.speed);
-
-  let newWeatherCondition = document.querySelector("#condition");
-  newWeatherCondition.innerHTML = response.data.weather[0].description;
-}
-
-// Entering city / location event
-let locationA = document.querySelector("#searchButton");
-locationA.addEventListener("submit", enterLocationButton);
-
-function clickFah(event) {
-  event.preventDefault();
-  let changeF = document.querySelector("#convertTemp");
-  changeF.innerHTML = `50`;
-}
-
-let fahR = document.querySelector("#degF");
-fahR.addEventListener("click", clickFah);
-
-function clickCel(event) {
-  event.preventDefault();
-  let changeC = document.querySelector("#convertTemp");
-  changeC.innerHTML = `10`;
-}
-let celC = document.querySelector("#degC");
-celC.addEventListener("click", clickCel);
-
-/* let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation); */
-
-//let description = document.querySelector("#id")
-// description.innerHTML = response.data.weather[0].description;
-
-// let cityElement = document.querySelector("#");
-// cityElement.innerHTML = cityId;
+axios.get(url).then(displayTemperature);
